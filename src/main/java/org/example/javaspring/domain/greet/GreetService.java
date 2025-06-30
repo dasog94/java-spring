27c73c63111beb24aecf5e32845391b2083f9dac
피레스 <dasog94@gmail.com>
2025-07-01 02:01:30 +0900

    temp gRPC
    server
    
    ㅇ

---
+++ src/main/java/org/example/javaspring/domain/greet/GreetService.java
@@ -0,0 +1,21 @@
+package org.example.javaspring.domain.greet;
+
+import io.grpc.stub.StreamObserver;
+import org.example.javaspring.domain.greet.proto.GreeterServiceGrpc;
+import org.example.javaspring.domain.greet.proto.HelloReply;
+import org.example.javaspring.domain.greet.proto.HelloRequest;
+import org.lognet.springboot.grpc.GRpcService;
+
+@GRpcService
+public class GreetService extends GreeterServiceGrpc.GreeterServiceImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder()
+                .setMessage("Hello " + request.getName())
+                .build();
+
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
